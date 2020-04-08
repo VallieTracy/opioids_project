a0c486e240f91211a25c7b86d840a076636ed40c
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-04-07 22:06:17 -0500

    have an app route, but it's not reading the db; might need to add p-key

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base 
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt 
+import numpy as np
+import pandas as pd
 
+engine = create_engine("sqlite:///../opioids_project/Resources/drugPoisoningDeaths.db")
 
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
 app = Flask(__name__)
 
 
@@ -8,7 +19,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
+    print("testing")
     return render_template('index.html')
 
 
