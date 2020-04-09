3e69ee931c28ae6c70ea24419053bd94b7b90d94
searanch <52631597+searanch@users.noreply.github.com>
2020-04-09 17:10:58 -0500

    updating

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+import datetime as dt
 
 
 app = Flask(__name__)
+
+engine = create_engine("sqlite:///Resources/drugPoisoningDeaths.db")
+# print(engine)
+Base = automap_base()
+# print(Base)
+Base.prepare(engine, reflect = True)
+deaths = Base.classes.drugPoisoningDeaths
+session = Session(engine)
+
 
 
 
