e135cbdf105d7c5a189dd7cae644ec508302e831
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-04-10 17:03:00 -0500

    work with geronimo, restoring my db

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -1,5 +1,15 @@
 var link = "http://127.0.0.1:5000/api/v1.0/deathTest";
+var d;
 
 d3.json(link).then(function(data){
-    console.log(data);
+    d = data;
+
+    var barData = [
+        {
+            x: data[7]["Deaths per 100,000 "]*100,
+            y: data[7]["State "]
+        }
+    ];
+
+    Plotly.newPlot('bar',barData);
 });
