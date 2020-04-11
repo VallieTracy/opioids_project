32743e5b145c9aab296c998ba259f54223ffd647
Melissa Keller <kelle595@umn.edu>
2020-04-11 10:56:25 -0500

    adding updates: working map and semi-working bar graph

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ from sqlalchemy import create_engine, func
 import datetime as dt 
 import numpy as np
 import pandas as pd
+from sqlalchemy.pool import SingletonThreadPool
 
 app = Flask(__name__)
 
-engine = create_engine("sqlite:///opioidsDB.db")
+engine = create_engine('sqlite:///opioidsDB.db',
+                        echo=True,
+                        connect_args={"check_same_thread": False})
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -20,39 +23,55 @@ prescriptions = Base.classes.prescriptions
 
 
 
+# session = Session(engine)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
+# Route to our about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+# Route to a dataset page
+@app.route('/deathsData')
+def deathsData():
+    return render_template('deathsData.html')
 
 @app.route('/api/v1.0/deathTest')
 def deathRoute():
     session = Session(engine)
-    deathByState = session.query(deaths.Location, deaths.Data)
+    deathByState = session.query(deaths.Location, deaths.Data, deaths.Fips, deaths.Drug_Type, deaths.TimeFrame)
     session.close()
 
     deathList = []
     for row in deathByState:
-        deathList.append({"State ": row[0], "Deaths per 100,000 ": row [1]})
+        deathList.append({"State": row[0], 
+                          "Deaths per 100,000": row [1],
+                          "Fips": row[2],
+                          "Drug Type": row[3],
+                          "Year": row[4]})
     
     return jsonify(deathList)
 
 @app.route('/api/v1.0/prescriptionTest')
 def prescriptionRoute():
     session = Session(engine)
-    prescriptionsByState = session.query(prescriptions.Location, prescriptions.Data)
+    prescriptionsByState = session.query(prescriptions.Location, prescriptions.Data, prescriptions.Fips, prescriptions.Oxy_Hydro, prescriptions.TimeFrame)
     session.close()
 
     presList = []
     for row in prescriptionsByState:
-        presList.append({"State ": row[0], "Prescriptions per 100,000 ": row[1]})
+        presList.append({"State": row[0], 
+                         "Prescriptions per 100,000": row[1],
+                         "Fips": row[2],
+                         "Oxycodone / Hydrocodone:": row[3],
+                         "Year": row[4]})
 
     return jsonify(presList)
 
-
+# session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -1,5 +1,35 @@
-var link = "http://127.0.0.1:5000/api/v1.0/prescriptionTest";
+// Creating map object
+
+var mymap = L.map('map')
+  .setView([38.27, -100.86], 4);
+
+// map.invalidateSize(true);
+
+// Adding tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: mak_api
+}).addTo(mymap);
+
+
+var link = "http://127.0.0.1:5000/api/v1.0/deathTest";
 
 d3.json(link).then(function(data){
-    console.log(data);
+  var d = data;
+
+  var barData = [
+      {
+          x: data["Deaths per 100,000"]*100,
+          y: data["State"]
+      }
+  ];
+
+  Plotly.newPlot('bar',barData);
+
+
 });
+
