b69f1a1430070d10f3421e1e7342f85862610b7b
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-04-16 20:05:44 -0500

    pushing before merging, deleted d3.json with remaining liz code

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -161,208 +161,12 @@ function yearUpdate(year){
 }
 // end yearUpdate Function
 
-d3.json(deathsUrl).then(function(deaths){
-  d3.json(salesUrl).then(function(sales){
 
-    for (var q=0; q<deaths.length; q++){
-        deaths[q]["Deaths per 100,000"] =+ deaths[q]["Deaths per 100,000"];
-        deaths[q]["Year"] =+ deaths[q]["Year"];
-    }
-
-    for (var q =0; q<sales.length; q++){
-        sales[q]["Year"] =+ sales[q]["Year"];
-        sales[q]["Prescriptions per 100,000"] =+ sales[q]["Prescriptions per 100,000"];
-    }
-
-    //LIZ SECTION
-
-    // Determine the range of year by building an object 
-    // and adding a key for each year. We've used a similar
-    // strategy in class activities.
-    var yearList = sales.map(s => s.Year);
-    yearList.sort();   
-    var yearDictionary = {}; 
-    yearList.forEach((year) => {
-      
-      if (year in yearDictionary)
-      {
-        yearDictionary[year]++; 
-      }
-      else
-      {
-        yearDictionary[year] = 1; 
-      }
-    }); 
-    // Show the format of the yearDictionary
-    // console.log("yearDictionary"); 
-    // console.log(yearDictionary); 
-    var deathData = [];
-    const yearKeys = Object.keys(yearDictionary);     
-    // For each year in the list of years ...
-    for (const yearKey of yearKeys) {
-      //console.log(deaths);
-      // ... filter out the Oxycodone values and sum them up for each state
-      var heroinData = deaths.filter(d => d["Drug Type"] === "Heroin" && d["Year"] == yearKey);
-      //console.log(heroinData);
-      var heroinSum = 0.0;
-      heroinData.forEach((item) => {
-        //console.log(item["Deaths per 100,000"])
-        if (isNaN(item["Deaths per 100,000"]) == false){
-          heroinSum += item["Deaths per 100,000"];
-        }
-      });
-
-      //Liz code for nat and semi deaths
-
-
-        //console.log(deaths);
-        // ... filter out the Oxycodone values and sum them up for each state
-        var natSemiData = deaths.filter(d => d["Drug Type"] === "Natural and semi-synthetic opioids" && d["Year"] == yearKey);
-        //console.log(natSemiData);
-        var natSemiSum = 0.0;
-        natSemiData.forEach((item) => {
-          //console.log(item["Deaths per 100,000"])
-          if (isNaN(item["Deaths per 100,000"]) == false){
-            natSemiSum += item["Deaths per 100,000"];
-          }
-        });
-
-
-          //console.log(deaths);
-          // ... filter out the Oxycodone values and sum them up for each state
-          var syntheticData = deaths.filter(d => d["Drug Type"] === "Synthetic opioids" && d["Year"] == yearKey);
-          
-          var syntheticSum = 0.0;
-          syntheticData.forEach((item) => {
-            //console.log(item["Deaths per 100,000"])
-            if (isNaN(item["Deaths per 100,000"]) == false){
-              syntheticSum += item["Deaths per 100,000"];
-            }
-          });
-
-      //console.log(heroinSum)
-      // console.log(`year: ${yearKey}, oxySum: ${oxySum}`); 
-      // console.log(`year: ${yearKey}, hydroSum: ${hydroSum}`); 
-      // Build a new dictionary containing the year, Oxycodone prescriptions, and Hydrocodone prescriptions
-      var deathDict = {}; 
-      deathDict["Year"] = yearKey;
-      deathDict["Heroin"] = heroinSum; 
-      deathDict["NatSemi"] = natSemiSum;
-      deathDict["Synthetic"]= syntheticSum;
-      
-
-      // Liz code trying to add deaths from opioids
-      //deathDict["NatSemi"] = natSemiSum;
-
-      // Finally, add this new dictionary to the array
-      deathData.push(deathDict); 
-    }
-    //console.log("DeathData"); 
-     
-
-    //State Stuff 
-
-    
-
-    //Liz Graph
-
-
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
-    chart.data = deathData; 
-
-
-    chart.dateFormatter.inputDateFormat = "yyyy";
-    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.minGridDistance = 60;
-    dateAxis.startLocation = 0.5;
-    dateAxis.endLocation = 0.5;
-    dateAxis.baseInterval = {
-      timeUnit: "Year",
-      count: 1
-    }
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.tooltip.disabled = true;
-    var series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.dateX = "Year";
-    series.name = "Heroin";
-    series.dataFields.valueY = "Heroin";
-    //series.tooltipHTML = "<img src='C:\Users\lizba\Desktop";
-    series.tooltipText = "[#000]{valueY.value}[/]";
-    series.tooltip.background.fill = am4core.color("#FFF");
-    series.tooltip.getStrokeFromObject = true;
-    series.tooltip.background.strokeWidth = 3;
-    series.tooltip.getFillFromObject = false;
-    series.fillOpacity = 0.6;
-    series.strokeWidth = 2;
-    series.stacked = true;
-    
-    
-    var series2 = chart.series.push(new am4charts.LineSeries());
-    series2.name = "NatSemi";
-    series2.dataFields.dateX = "Year";
-    series2.dataFields.valueY = "NatSemi";
-    series2.tooltipHTML = "<img src='https://www.amcharts.com/lib/3/images/motorcycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>{valueY.value}</b></span>";
-    series2.tooltipText = "[#000]{valueY.value}[/]";
-    series2.tooltip.background.fill = am4core.color("#FFF");
-    series2.tooltip.getFillFromObject = false;
-    series2.tooltip.getStrokeFromObject = true;
-    series2.tooltip.background.strokeWidth = 3;
-    series2.sequencedInterpolation = true;
-    series2.fillOpacity = 0.6;
-    series2.stacked = true;
-    series2.strokeWidth = 2;
-    
-    
-    var series3 = chart.series.push(new am4charts.LineSeries());
-    series3.name = "Synthetic";
-    series3.dataFields.dateX = "Year";
-    series3.dataFields.valueY = "Synthetic";
-    series3.tooltipHTML = "<img src='https://www.amcharts.com/lib/3/images/bicycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>{valueY.value}</b></span>";
-    series3.tooltipText = "[#000]{valueY.value}[/]";
-    series3.tooltip.background.fill = am4core.color("#FFF");
-    series3.tooltip.getFillFromObject = false;
-    series3.tooltip.getStrokeFromObject = true;
-    series3.tooltip.background.strokeWidth = 3;
-    series3.sequencedInterpolation = true;
-    series3.fillOpacity = 0.6;
-    series3.defaultState.transitionDuration = 1000;
-    series3.stacked = true;
-    series3.strokeWidth = 2;
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.xAxis = dateAxis;
-    chart.scrollbarX = new am4core.Scrollbar();
-    
-    // Add a legend
-    chart.legend = new am4charts.Legend();
-    chart.legend.position = "top";
-    // axis ranges
-    var range = dateAxis.axisRanges.create();
-    range.date = new Date(2001, 0, 1);
-    range.endDate = new Date(2003, 0, 1);
-    range.axisFill.fill = chart.colors.getIndex(7);
-    range.axisFill.fillOpacity = 0.2;
-    //range.label.text = "Fines for speeding increased";
-    range.label.inside = true;
-    range.label.rotation = 90;
-    range.label.horizontalCenter = "right";
-    range.label.verticalCenter = "bottom";
-    var range2 = dateAxis.axisRanges.create();
-    range2.date = new Date(2007, 0, 1);
-    range2.grid.stroke = chart.colors.getIndex(7);
-    range2.grid.strokeOpacity = 0.6;
-    range2.grid.strokeDasharray = "5,2";
-    //range2.label.text = "Motorcycle fee introduced";
-    range2.label.inside = true;
-    range2.label.rotation = 90;
-    range2.label.horizontalCenter = "right";
-    range2.label.verticalCenter = "bottom";
-  }) //end of sales json
-
-}); //end of death json
 
 
 function radialChart(curState) {   
-  var chartTitle = d3.select("#radialChartTitle").text(`${curState} Prescription Sales Per 100,000 People`);
+  var titleStr = `${curState} Prescription Sales`;
+  var chartTitle = d3.select("#radialChartTitle").html(titleStr +  "\<br\>Per 100,000 People");
   let filterData = salesRadialData; 
   
   filterData = filterData.filter(d => d.State === curState);
@@ -393,7 +197,7 @@ function radialChart(curState) {
       // Filter out the oxycodone sales
       var oxyData = filterData.filter(d => d["Oxycodone / Hydrocodone"] === "Oxycodone" && d["Year"] == yearSKey);
       var oxy = oxyData[0]["Prescriptions per 100,000"];
-      
+            
       // Filter out the Hydrocodone sales
       var hydroData = filterData.filter(d => d["Oxycodone / Hydrocodone"] === "Hydrocodone" && d["Year"] == yearSKey);
       var hydro = hydroData[0]["Prescriptions per 100,000"]; 
@@ -407,7 +211,6 @@ function radialChart(curState) {
       newSData.push(newSDict); 
     }
     
-
     // Vallie's radial chart
     //Chart code 
     /* Create chart instance */
@@ -460,7 +263,6 @@ function radialChart(curState) {
 
 function stateChange() {
   am4core.disposeAllCharts();
-  //chart.dispose();
   let curState = this.value;
   radialChart(curState);
 }
